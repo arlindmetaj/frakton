6b77fff40e429dc8416691cc0c11f5b26a001c5a
Iglibogdani <iglibo18@gmail.com>
2022-03-23 14:24:05 +0100

    working in lunch break

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
-import 'package:location/location.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dialogs/flutter_dialogs.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:frakton_task/Screens/Authentication/login_screen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-const LatLng SOURCE_LOCATION = LatLng(41.327953, 19.819025);
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,41 +15,193 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final LatLng _initialCameraPosition = const LatLng(20.5937, 78.9629);
-  late GoogleMapController _controller;
-  final Location _location = Location();
+  final LatLng initialCameraPosition = const LatLng(41.327953, 19.819025);
+  final Completer<GoogleMapController> _controller2 = Completer();
 
-  void _onMapCreated(GoogleMapController _cntrl) {
-    _controller = _cntrl;
-    _location.onLocationChanged.listen((l) {
-      _controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: LatLng(
-              l.latitude!,
-              l.longitude!,
-            ),
-            zoom: 15,
-          ),
-        ),
-      );
-    });
-  }
+  //final Location _location = Location();
+
+  double zoomVal = 5.0;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Stack(
-            children: [
-              GoogleMap(
-                initialCameraPosition: CameraPosition(target: _initialCameraPosition),
-                mapType: MapType.normal,
-                onMapCreated: _onMapCreated,
-                myLocationEnabled: true,
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.redAccent,
+          centerTitle: true,
+          title: const Text('MAP VIEW'),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(15),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.exit_to_app,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  logOutDialog(context);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            googleMap(context),
+            // zoomMinusFunction(),
+            // zoomPlusFunction(),
+            _buildFavorites(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget googleMap(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        zoomGesturesEnabled: true,
+        tiltGesturesEnabled: false,
+        initialCameraPosition: const CameraPosition(
+            target: LatLng(40.712776, -74.005974), zoom: 12),
+        onMapCreated: (GoogleMapController controller) {
+          _controller2.complete(controller);
+        },
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        markers: {
+          newYork1Marker,
+          newYork2Marker,
+          newYork3Marker,
+          gramercyMarker,
+          bernardinMarker,
+          blueMarker
+        },
+      ),
+    );
+  }
+
+  Widget zoomMinusFunction() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+          icon: const Icon(FontAwesomeIcons.magnifyingGlassMinus,
+              color: Color(0xff6200ee)),
+          onPressed: () {
+            zoomVal--;
+            _minus(zoomVal);
+          }),
+    );
+  }
+
+  Widget zoomPlusFunction() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+          icon: const Icon(FontAwesomeIcons.magnifyingGlassPlus,
+              color: Color(0xff6200ee)),
+          onPressed: () {
+            zoomVal++;
+            _plus(zoomVal);
+          }),
+    );
+  }
+
+  Future<void> _minus(double zoomVal) async {
+    final GoogleMapController controller = await _controller2.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: const LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  }
+
+  Future<void> _plus(double zoomVal) async {
+    final GoogleMapController controller = await _controller2.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: const LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  }
+
+  Widget _buildFavorites() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 100),
+        height: 150.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            const SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://eachlittleworld.typepad.com/.a/6a00e554503eee883301a73d98a83c970d-800wi",
+                  40.738380,
+                  -73.988426,
+                  "Gramercy Tavern"),
+            ),
+            const SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipMKRN-1zTYMUVPrH-CcKzfTo6Nai7wdL7D8PMkt=w340-h160-k-no",
+                  40.761421,
+                  -73.981667,
+                  "Le Bernardin"),
+            ),
+            const SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://images.unsplash.com/photo-1504940892017-d23b9053d5d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                  40.732128,
+                  -73.999619,
+                  "Blue Hill"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _boxes(String _image, double lat, double long, String restaurantName) {
+    return GestureDetector(
+      onTap: () {
+        gotoLocation(lat, long);
+      },
+      child: FittedBox(
+        child: Material(
+          color: Colors.white,
+          elevation: 14.0,
+          borderRadius: BorderRadius.circular(24.0),
+          shadowColor: const Color(0x802196F3),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                width: 180,
+                height: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(24.0),
+                  child: Image(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(_image),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: myDetailsContainer1(restaurantName),
               ),
             ],
           ),
@@ -58,12 +210,185 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Future<void> logOut(BuildContext context) async {
-    await FirebaseAuth.instance.signOut();
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (context) => const LoginScreen(),
+  Widget myDetailsContainer1(String restaurantName) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            restaurantName,
+            style: const TextStyle(
+                color: Color(0xff6200ee),
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(height: 5.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const <Widget>[
+            Text(
+              "4.1",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18.0,
+              ),
+            ),
+            Icon(
+              FontAwesomeIcons.solidStar,
+              color: Colors.amber,
+              size: 15.0,
+            ),
+            Icon(
+              FontAwesomeIcons.solidStar,
+              color: Colors.amber,
+              size: 15.0,
+            ),
+            Icon(
+              FontAwesomeIcons.solidStar,
+              color: Colors.amber,
+              size: 15.0,
+            ),
+            Icon(
+              FontAwesomeIcons.solidStar,
+              color: Colors.amber,
+              size: 15.0,
+            ),
+            Icon(
+              FontAwesomeIcons.solidStarHalf,
+              color: Colors.amber,
+              size: 15.0,
+            ),
+            Text(
+              "(946)",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18.0,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 5.0),
+        const Text(
+          "American \u00B7 \u0024\u0024 \u00B7 1.6 mi",
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 18.0,
+          ),
+        ),
+        const SizedBox(height: 5.0),
+        const Text(
+          "Closed \u00B7 Opens 17:00 Thu",
+          style: TextStyle(
+              color: Colors.black54,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Future<void> gotoLocation(double lat, double long) async {
+    final GoogleMapController controller = await _controller2.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(lat, long),
+          zoom: 15,
+          tilt: 50.0,
+          bearing: 45.0,
+        ),
       ),
     );
   }
+}
+
+Marker gramercyMarker = Marker(
+  markerId: const MarkerId('gramercy'),
+  position: const LatLng(40.738380, -73.988426),
+  infoWindow: const InfoWindow(title: 'Gramercy Tavern'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+Marker bernardinMarker = Marker(
+  markerId: const MarkerId('bernardin'),
+  position: const LatLng(40.761421, -73.981667),
+  infoWindow: const InfoWindow(title: 'Le Bernardin'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker blueMarker = Marker(
+  markerId: const MarkerId('bluehill'),
+  position: const LatLng(40.732128, -73.999619),
+  infoWindow: const InfoWindow(title: 'Blue Hill'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+//New York Marker
+
+Marker newYork1Marker = Marker(
+  markerId: const MarkerId('newyork1'),
+  position: const LatLng(40.742451, -74.005959),
+  infoWindow: const InfoWindow(title: 'Los Tacos'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker newYork2Marker = Marker(
+  markerId: const MarkerId('newyork2'),
+  position: const LatLng(40.729640, -73.983510),
+  infoWindow: const InfoWindow(title: 'Tree Bistro'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker newYork3Marker = Marker(
+  markerId: const MarkerId('newyork3'),
+  position: const LatLng(40.719109, -74.000183),
+  infoWindow: const InfoWindow(title: 'Le Coucou'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+void logOutDialog(BuildContext context) {
+  showPlatformDialog(
+    context: context,
+    builder: (context) => BasicDialogAlert(
+      title: const Text("Log Out"),
+      content: const Text("Are you sure ?"),
+      actions: <Widget>[
+        BasicDialogAction(
+          title: const Text("Cancel"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        BasicDialogAction(
+          title: const Text(
+            "Log Out",
+            style: TextStyle(color: Colors.red),
+          ),
+          onPressed: () {
+            logOut(context);
+          },
+        ),
+      ],
+    ),
+  );
+}
+
+Future<void> logOut(BuildContext context) async {
+  await FirebaseAuth.instance.signOut();
+  Navigator.of(context).pushReplacement(
+    MaterialPageRoute(
+      builder: (context) => const LoginScreen(),
+    ),
+  );
 }
